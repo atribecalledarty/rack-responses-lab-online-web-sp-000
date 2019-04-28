ad0f37c0491fc9b6aee8bae5fe9dac2652a6e338
Arthur Song <arth3rs0ng@gmail.com>
2019-04-28 22:54:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,11 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    resp.write Time.new.hour
+    if Time.new.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
     
     resp.finish
   end
